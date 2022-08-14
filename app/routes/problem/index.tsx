89e48f742a4f3217d4c4f8b89eb2377cf9b5ebba
joelazar <jozsef.gabor.lazar@gmail.com>
2022-08-14 19:10:21 +0200

    :sparkles: use new loader signature

--- app/routes/problem/index.tsx
+++ app/routes/problem/index.tsx
@@ -1,8 +1,10 @@
-import type { Note } from "@prisma/client";
 import { useLoaderData } from "@remix-run/react";
-import type { LoaderFunction } from "@remix-run/server-runtime";
+import type { LoaderArgs } from "@remix-run/server-runtime";
 import format from "date-fns/format";
 import { prisma } from "~/db.server";
+
+/*
+import type { Note } from "@prisma/client";
 
 type LoaderData = {
   notes: Note[];
@@ -12,9 +14,16 @@ export const loader: LoaderFunction = async () => {
   const notes = await prisma.note.findMany({});
   return { notes };
 };
+*/
+
+export const loader = async (args: LoaderArgs) => {
+  const notes = await prisma.note.findMany({});
+  return { notes };
+};
 
 export default function Problem() {
-  const { notes } = useLoaderData<LoaderData>();
+  /* const { notes } = useLoaderData<LoaderData>(); */
+  const { notes } = useLoaderData<typeof loader>();
 
   return (
     <>
@@ -25,15 +34,15 @@ export default function Problem() {
         >
           <h2 className="text-2xl font-bold ">{note.title}</h2>
           <ul className="text-lg">
-            {/* <li>Created at: {format(note.createdAt, "yyyy-MM-dd")}</li> */}
-            {/* <li>Updated at: {format(note.updatedAt, "yyyy-MM-dd")}</li> */}
+            <li>Created at: {format(note.createdAt, "yyyy-MM-dd")}</li>
+            <li>Updated at: {format(note.updatedAt, "yyyy-MM-dd")}</li>
             {/* stupid solution */}
-            <li>
-              Created at: {format(new Date(note.createdAt), "yyyy-MM-dd")}
-            </li>
-            <li>
-              Updated at: {format(new Date(note.updatedAt), "yyyy-MM-dd")}
-            </li>
+            {/* <li> */}
+            {/*   Created at: {format(new Date(note.createdAt), "yyyy-MM-dd")} */}
+            {/* </li> */}
+            {/* <li> */}
+            {/*   Updated at: {format(new Date(note.updatedAt), "yyyy-MM-dd")} */}
+            {/* </li> */}
             <li>Body: {note.body}</li>
           </ul>
         </div>
