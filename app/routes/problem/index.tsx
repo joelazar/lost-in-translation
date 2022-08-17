0272614e255ce10e2597402b0766b70776a7043f
joelazar <jozsef.gabor.lazar@gmail.com>
2022-08-17 20:52:17 +0200

    :recycle: replace date-fns call with broser DateTimeFormat function

--- app/routes/problem/index.tsx
+++ app/routes/problem/index.tsx
@@ -1,7 +1,6 @@
 import type { Note } from "@prisma/client";
-import { useLoaderData } from "@remix-run/react";
+import { useCatch, useLoaderData } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/server-runtime";
-import format from "date-fns/format";
 import { prisma } from "~/db.server";
 
 type LoaderData = {
@@ -34,10 +33,43 @@ export default function Problem() {
             <li>
               Updated at: {format(new Date(note.updatedAt), "yyyy-MM-dd")}
             </li>
+            <li>Created at: {format(note.createdAt, "yyyy-MM-dd")}</li>
+            <li>Updated at: {format(note.updatedAt, "yyyy-MM-dd")}</li>
+            <li>
+              Created at: {Intl.DateTimeFormat("en-US").format(note.createdAt)}
+            </li>
+            <li>
+              Updated at: {Intl.DateTimeFormat("en-US").format(note.updatedAt)}
+            </li>
             <li>Body: {note.body}</li>
           </ul>
         </div>
       ))}
     </>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <div className="bg-yellow-100">
+      <h1>Caught</h1>
+      <p>Status: {caught.status}</p>
+      <pre>
+        <code>{JSON.stringify(caught.data, null, 2)}</code>
+      </pre>
+    </div>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <div className="bg-red-100">
+      <h1>My error boundary</h1>
+      <p>{error.message}</p>
+      <p>The stack trace is:</p>
+      <pre>{error.stack}</pre>
+    </div>
   );
 }
