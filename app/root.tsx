0e2a1a575b55a4ea15828ed3ea70cfb5fdf7c341
joelazar <jozsef.gabor.lazar@gmail.com>
2022-08-18 15:04:39 +0200

    :bug: fix title

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Lost in translation",
   viewport: "width=device-width,initial-scale=1",
 });
 
